ab7f517fbdce558cb5126bd3068df79ac15984e1
Siyn Ma <mrtatekii33@gmail.com>
2022-05-26 11:48:25 +0800

    feat(pwa): pwa

--- src/components/cart-dropdown/cart-dropdown.tsx
+++ src/components/cart-dropdown/cart-dropdown.tsx
@@ -4,14 +4,13 @@ import { observer } from "mobx-react-lite";
 import CartDropdownList from "./cart-dropdown-list";
 import { useNavigate } from "react-router-dom";
 import { CartDropdownContainer } from "./cart-dropdown.styles";
-import { useCallback } from "react";
 
 const CartDropdown = () => {
   const { cartIsOpen, cartItemsCount } = useCartStore();
   const navigate = useNavigate();
 
   // 跳转到结算
-  const jumpToCheckout = useCallback(() => navigate("/checkout"), []);
+  const jumpToCheckout = () => navigate("/checkout");
 
   if (!cartIsOpen) return null; // 未打开直接return
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RootStoreProvider } from "./store";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -22,6 +23,7 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
